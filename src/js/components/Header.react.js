2f55d5ae56550e6c0ce7e8f1f86844da812b4bc3
Nicolas Santini <nsantini@gmail.com>
2015-11-22 19:59:59 +1300

    initial commit

---
+++ src/js/components/Header.react.js
@@ -0,0 +1,36 @@
+var React = require('react');
+var TextInput = require('./TextInput.react');
+var Actions = require('../actions/Actions');
+
+var Header = React.createClass({
+
+    render: function() {
+        return (
+            <header id="header">
+            <div className="jumbotron text-center">
+                <h1>Tasker</h1>
+                <p className="lead">
+                    The very simple task manager. &nbsp;
+                    <a href="https://github.com/nsantini/tasker" target="_blank">
+                        <span class="username">source</span>
+                    </a>
+                </p>
+                <TextInput
+                    id="new-todo"
+                    placeholder="What shall we do today?"
+                    onSave={this._onSave}
+                />
+            </div>`
+      </header>
+    );
+  },
+
+  _onSave: function(text) {
+
+    if (text.trim()) {
+      Actions.create(text);
+    }
+  }
+});
+
+module.exports = Header;
---
+++ src/js/stores/Store.js
@@ -0,0 +1,101 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var Dispatcher = require('../dispatcher/Dispatcher');
+
+var Store = assign({}, EventEmitter.prototype, {
+
+  getAllTodos: function() {
+    var objects = _.map(_.values(localStorage), function(todo) {
+      try {return JSON.parse(todo);
+      } catch (err) {
+        return false;
+      }
+    });
+    var todos = _.filter(objects, function(todo) {
+      return todo && todo.id && todo.id.startsWith('today_');
+    });
+
+    return todos;
+  },
+
+  emitChange: function() {
+    this.emit('changed');
+  },
+
+  addListener: function(callback) {
+    this.on('changed', callback);
+  },
+
+  removeListener: function(callback) {
+    this.removeListener('changed', callback);
+  },
+
+  create: function(text) {
+    var id = 'today_' + Date.now();
+    localStorage.setItem(id, JSON.stringify({
+      id: id,
+      text: text,
+      completed: false,
+      added: Date.now()
+    }));
+  },
+
+  toggle: function(id) {
+    var todo = JSON.parse(localStorage.getItem(id));
+    todo.completed = !todo.completed;
+    localStorage.setItem(id, JSON.stringify(todo));
+  },
+
+  toggleAll: function() {
+    _.each(_.filter(_.keys(localStorage), function(key) {
+      return key.startsWith('today_');
+    }), this.toggle);
+  },
+
+  delete: function(id) {
+    localStorage.removeItem(id);
+  },
+
+  deleteAll: function() {
+    _.each(_.filter(_.keys(localStorage), function(key) {
+      return key.startsWith('today_');
+    }), this.delete);
+  }
+});
+
+Dispatcher.register(function(action) {
+  switch (action.action) {
+    case 'LOAD':
+      Store.emitChange();
+      break;
+
+    case 'CREATE':
+      Store.create(action.text);
+      Store.emitChange();
+      break;
+
+    case 'TOGGLE':
+      Store.toggle(action.id);
+      Store.emitChange();
+      break;
+
+    case 'TOGGLEALL':
+      Store.toggleAll();
+      Store.emitChange();
+      break;
+
+    case 'DELETE':
+      Store.delete(action.id);
+      Store.emitChange();
+      break;
+
+    case 'DELETEALL':
+      Store.deleteAll();
+      Store.emitChange();
+      break;
+  }
+});
+
+module.exports = Store;
